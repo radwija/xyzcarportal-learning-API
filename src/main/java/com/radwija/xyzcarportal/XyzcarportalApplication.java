1555a7231970ecd4092a276a0ea0578b0344c04c
radwija <115165737+radwija@users.noreply.github.com>
2023-05-12 17:56:41 +0800

    first init

---
+++ src/main/java/com/radwija/xyzcarportal/XyzcarportalApplication.java
@@ -0,0 +1,13 @@
+package com.radwija.xyzcarportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzcarportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzcarportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radwija/xyzcarportal/XyzcarportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radwija.xyzcarportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzcarportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
